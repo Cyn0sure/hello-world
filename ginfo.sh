7ab18bdceaf8f82b595e0489a9eda0948a68e5ff
Z Wei <zxweitju@gmail.com>
2015-07-19 19:18:31 -0400

    add a sentence to show the current path

--- ginfo.sh
+++ ginfo.sh
@@ -5,4 +5,10 @@ echo "Today is \c";date
 echo "Number of user login : \c" ; who | wc -l
 echo "Calendar"
 cal 
+myname=viki
+myos=windows
+echo "My name is $myname"
+echo "My os is $myos"
+mypath=pwd
+echo "My path is `pwd`"
 exit 0
