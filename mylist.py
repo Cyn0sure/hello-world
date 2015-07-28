915913bccb155a5ccab3f29e95a829579de65d9c
Z Wei <zxweitju@gmail.com>
2015-07-28 19:24:38 -0400

    add My Project in readme, add one more line in readme; list tenant in MyClass()

--- mylist.py
+++ mylist.py
@@ -4,6 +4,6 @@ class MyClass(object):
 
     tenant={'name':'zx','id':'ed45'}
 
-instance_list = [MyClass() for i in range(10)]
+instance_list = [MyClass().tenant for i in range(10)]
 
 print dir(instance_list[1])
